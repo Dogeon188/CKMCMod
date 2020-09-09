920751a0509fe29bb6ecd431c14e84e709a1afd3
Dogeon188 <dogcatdog188@gmail.com>
2020-09-09 19:26:52 +0800

    setups

--- src/main/java/me/ckffmc/farm/MainMod.java
+++ src/main/java/me/ckffmc/farm/MainMod.java
@@ -2,13 +2,16 @@ package me.ckffmc.farm;
 
 import net.fabricmc.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class MainMod implements ModInitializer {
+
+	public static final String MOD_ID = "ckfarm";
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		System.out.println("Hello Fabric world!");
+		System.out.println("Hello world!");
 	}
 }
