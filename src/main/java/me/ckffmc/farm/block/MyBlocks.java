194704462412c63e65c92d07564448c1650cdad8
Dogeon188 <dogcatdog188@gmail.com>
2020-09-20 20:54:27 +0800

    lettuce & sweet potato (abstract crop class for age-3 and age-7 crops) age-3 -> EarthCropSmallBlock age-7 -> EarthCropBlock also some typos fixed

--- src/main/java/me/ckffmc/farm/block/MyBlocks.java
+++ src/main/java/me/ckffmc/farm/block/MyBlocks.java
@@ -1,13 +1,18 @@
 package me.ckffmc.farm.block;
 
 import me.ckffmc.farm.MainMod;
+import me.ckffmc.farm.block.crop.EarthCropBlock;
+import me.ckffmc.farm.block.crop.EarthCropSmallBlock;
+import me.ckffmc.farm.item.MyItems;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.*;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class MyBlocks {
+    public static final Block LETTUCE;
+    public static final Block SWEET_POTATO;
     public static final Block DEBUG_BLOCK;
 
     private static Block register(String id, Block block) {
@@ -15,6 +20,8 @@ public class MyBlocks {
     }
 
     static {
+        LETTUCE = register("lettuce", new EarthCropBlock(MyItems.LETTUCE_SEEDS, AbstractBlock.Settings.of(Material.PLANT).noCollision().ticksRandomly().breakInstantly().sounds(BlockSoundGroup.CROP)));
+        SWEET_POTATO = register("sweet_potato", new EarthCropSmallBlock(MyItems.SWEET_POTATO, AbstractBlock.Settings.of(Material.PLANT).noCollision().ticksRandomly().breakInstantly().sounds(BlockSoundGroup.CROP)));
         DEBUG_BLOCK = register("debug_block", new Block(FabricBlockSettings.copyOf(Blocks.STONE)));
     }
 }
