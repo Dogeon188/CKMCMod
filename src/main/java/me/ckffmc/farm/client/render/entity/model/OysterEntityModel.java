34ac1a73bf056eeca6ab1d09db427a4bcef82ccf
Dogeon188 <dogcatdog188@gmail.com>
2020-10-13 13:46:14 +0800

    oyster model optimize

--- src/main/java/me/ckffmc/farm/client/render/entity/model/OysterEntityModel.java
+++ src/main/java/me/ckffmc/farm/client/render/entity/model/OysterEntityModel.java
@@ -16,15 +16,15 @@ public class OysterEntityModel extends CompositeEntityModel<OysterEntity> {
         this.textureWidth = 32;
         this.textureHeight = 32;
 
-        this.torso = new ModelPart(this, 0, 14);
-        this.torso.addCuboid(-2.0F, 0.5F, -1.0F, 4.0F, 1.0F, 2.0F, 0.0F);
+        this.torso = new ModelPart(this, 0, 14)
+                .addCuboid(-2.0F, 0.5F, -1.0F, 4.0F, 1.0F, 2.0F);
         this.torso.setPivot(0.0F, 22.0F, 0.0F);
-        this.bottomShell = new ModelPart(this, 0, 7);
-        this.bottomShell.addCuboid(-4.0F, 1.0F, -3.0F, 8.0F, 1.0F, 6.0F, 0.0F);
+        this.bottomShell = new ModelPart(this, 0, 7)
+                .addCuboid(-4.0F, 1.0F, -3.0F, 8.0F, 1.0F, 6.0F);
         this.bottomShell.setPivot(0.0F, 22.0F, 0.0F);
         this.bottomShell.yaw = 3.1415926F;
-        this.topShell = new ModelPart(this, 0, 0);
-        this.topShell.addCuboid(-4.0F, 0.0F, 0.0F, 8.0F, 1.0F, 6.0F, 0.0F);
+        this.topShell = new ModelPart(this, 0, 0)
+                .addCuboid(-4.0F, 0.0F, 0.0F, 8.0F, 1.0F, 6.0F);
         this.topShell.setPivot(0.0F, 23.0F, 3.0F);
     }
 
@@ -34,6 +34,6 @@ public class OysterEntityModel extends CompositeEntityModel<OysterEntity> {
     @Override
     public void setAngles(OysterEntity entity, float limbAngle, float limbDistance, float animationProgress, float headYaw, float headPitch) {
         this.topShell.pitch = ModelUtil.interpolateAngle(3.08F, 3.1415926F,
-                MathHelper.sin((animationProgress % 128.0f) / 20.371832715f - 3.1415926f));
+                MathHelper.sin((animationProgress % 128.0F) / 20.371832715F - 3.1415926F));
     }
 }
