d30a21cf514a5428645f9278c4646a0b4edacc3a
Dogeon188 <dogcatdog188@gmail.com>
2020-11-11 23:17:08 +0800

    villager interaction with new crops

---
+++ src/main/java/me/ckffmc/farm/mixin/entity/FarmerWorkTaskAccessor.java
@@ -0,0 +1,17 @@
+package me.ckffmc.farm.mixin.entity;
+
+import net.minecraft.entity.ai.brain.task.FarmerWorkTask;
+import net.minecraft.item.Item;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.List;
+
+@Mixin(FarmerWorkTask.class)
+public interface FarmerWorkTaskAccessor {
+    @Accessor("COMPOSTABLES")
+    static void setCompostables(List<Item> items) { throw new AssertionError(); }
+
+    @Accessor("COMPOSTABLES")
+    static List<Item> getCompostables() { throw new AssertionError(); }
+}
